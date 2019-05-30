10ed39816baad5ed96ec48a35f25b1ff5de018a5
pei <devbypei@gmail.com>
2019-05-30 09:27:24 +0800

    init

---
+++ src/main/java/cc/gooto/hello/HelloApplication.java
@@ -0,0 +1,21 @@
+package cc.gooto.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloApplication.class, args);
+	}
+
+	@GetMapping(value = "hello")
+	public String name() {
+		return "Hello:Pi";
+	}
+
+}
